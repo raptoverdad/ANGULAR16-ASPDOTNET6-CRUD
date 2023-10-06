fdc4e01ae0893dbbb684b7ac0e531b4541c67321
Rodrigo Iván Miranda Reidenbach <rodrigomreidenbach@gmail.com>
2023-10-06 15:01:07 -0300

    fixin' swagger server url

--- angular-application/src/app/api/api-configuration.ts
+++ angular-application/src/app/api/api-configuration.ts
@@ -9,7 +9,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class ApiConfiguration {
-  rootUrl: string = 'http://a6f65202cff19422abf34be6d617f0d8-1921506352.sa-east-1.elb.amazonaws.com';
+  rootUrl: string = 'http://a6f65202cff19422abf34be6d617f0d8-1921506352.sa-east-1.elb.amazonaws.com:6000';
 }
 
 /**
